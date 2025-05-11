d0323c8aa80b88c2db826050baf6a3d5e4dced95
Hemant Saw <sawhemant41@gmail.com>
2025-05-11 20:15:24 +0530

    Modified output file for sonarqube

--- Terraform-Infra/outputs.tf
+++ Terraform-Infra/outputs.tf
@@ -1,3 +1,8 @@
 output "jenkins_server_public_ip" {
     value =  "Your Jenkins Server IP is : ${aws_instance.jenkins_server.public_ip}"
 }
+
+output "sonarqube_server_public_ip" {
+    value =  "Your SonarQube Server IP is : ${aws_instance.sonarqube_server.public_ip}"
+  
+}
